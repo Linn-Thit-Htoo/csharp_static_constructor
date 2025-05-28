0fa0c2a6d678cd597e0e859aad2fefe9f52176df
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-28 22:10:30 +0630

    Add project files.

---
+++ csharp_static_constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(AppConfig.Setting);
+        AppConfig.PrintSetting();
+    }
+}
+
+public class AppConfig
+{
+    public static string Setting;
+
+    static AppConfig()
+    {
+        Console.WriteLine("Static constructor called");
+        Setting = "DefaultConfig";
+    }
+
+    public static void PrintSetting()
+    {
+        Console.WriteLine(Setting);
+    }
+}
